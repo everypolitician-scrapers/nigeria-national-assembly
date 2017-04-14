61fec4fa47901818667404aa2802bca3074b7d86
Tony Bowden <tony@tmtm.com>
2017-04-14 06:11:27 +0100

    Reformat extra member_urls section

--- scraper.rb
+++ scraper.rb
@@ -114,54 +114,30 @@ end.uniq
 # The search index and list pages seem to be missing some members of
 # the National Assembly, so add them manually to the pages to be scraped:
 member_urls |= [
-  # Hon. AWAJI-INOMBEK DAGOMIE ABIANTE
-  'http://www.nass.gov.ng/mp/profile/836',
-  # Hon. BELLO ABDULLAHI
-  'http://www.nass.gov.ng/mp/profile/664',
-  # Hon. BROWN RANDOLPH IWO ONYERE
-  'http://www.nass.gov.ng/mp/profile/896',
-  # Hon. DEKOR ROBINSON DUMNAMENE
-  'http://www.nass.gov.ng/mp/profile/538',
-  # Hon. Dennis Nnamdi Agbo
-  'http://www.nass.gov.ng/mp/profile/635',
-  # Hon. EMERENGWA SUNDAY BONIFACE
-  'http://www.nass.gov.ng/mp/profile/631',
-  # Hon. GOGO BRIGHT TAMUNO
-  'http://www.nass.gov.ng/mp/profile/884',
-  # Hon. JACOBSON NBINA BARINEKA
-  'http://www.nass.gov.ng/mp/profile/527',
-  # Hon. KENNETH ANAYO CHIKERE
-  'http://www.nass.gov.ng/mp/profile/839',
-  # Hon. KWAMOTI BITRUS LAORI
-  'http://www.nass.gov.ng/mp/profile/646',
-  # Hon. NSIEGBE BLESSING IBIBA
-  'http://www.nass.gov.ng/mp/profile/306',
-  # Hon. Betty Jocelyn Apiafi
-  'http://www.nass.gov.ng/mp/profile/675',
-  # Hon. D Goodhead Boma
-  'http://www.nass.gov.ng/mp/profile/826',
-  # Hon. Ugwuegede Ikechukwu
-  'http://www.nass.gov.ng/mp/profile/952',
-  # Hon. Jerome Eke
-  'http://www.nass.gov.ng/mp/profile/831',
-  # Hon. CHINDA KINGSLEY OGUNDU
-  'http://www.nass.gov.ng/mp/profile/302',
-  # Sen. GEORGE THOMPSON SEKIBO
-  'http://www.nass.gov.ng/mp/profile/876',
-  # Sen. LILIAN UCHE EKWUNIFE
-  'http://www.nass.gov.ng/mp/profile/91',
-  # Sen. OLAKA JOHNSON NWOGU
-  'http://www.nass.gov.ng/mp/profile/937',
-  # Sen. Ahmed .Salau Ogembe
-  'http://nass.gov.ng/mp/profile/891',
-  # Sen. OHUABUNWA AZIKIWE MAO
-  'http://nass.gov.ng/mp/profile/506',
-  # Sen. ABDUL ABDULRAHMAN ABUBAKAR
-  'http://www.nass.gov.ng/mp/profile/520',
-  # Sen. OSINAKACHUKWU T IDEOZU
-  'http://www.nass.gov.ng/mp/profile/812',
-  # Sen. HUSSAIN EGYE SALIHU
-  'http://www.nass.gov.ng/mp/profile/519',
+  'http://www.nass.gov.ng/mp/profile/91',  # Sen. LILIAN UCHE EKWUNIFE
+  'http://www.nass.gov.ng/mp/profile/302', # Hon. CHINDA KINGSLEY OGUNDU
+  'http://www.nass.gov.ng/mp/profile/306', # Hon. NSIEGBE BLESSING IBIBA
+  'http://www.nass.gov.ng/mp/profile/506', # Sen. OHUABUNWA AZIKIWE MAO
+  'http://www.nass.gov.ng/mp/profile/519', # Sen. HUSSAIN EGYE SALIHU
+  'http://www.nass.gov.ng/mp/profile/520', # Sen. ABDUL ABDULRAHMAN ABUBAKAR
+  'http://www.nass.gov.ng/mp/profile/527', # Hon. JACOBSON NBINA BARINEKA
+  'http://www.nass.gov.ng/mp/profile/538', # Hon. DEKOR ROBINSON DUMNAMENE
+  'http://www.nass.gov.ng/mp/profile/631', # Hon. EMERENGWA SUNDAY BONIFACE
+  'http://www.nass.gov.ng/mp/profile/635', # Hon. Dennis Nnamdi Agbo
+  'http://www.nass.gov.ng/mp/profile/646', # Hon. KWAMOTI BITRUS LAORI
+  'http://www.nass.gov.ng/mp/profile/664', # Hon. BELLO ABDULLAHI
+  'http://www.nass.gov.ng/mp/profile/675', # Hon. Betty Jocelyn Apiafi
+  'http://www.nass.gov.ng/mp/profile/812', # Sen. OSINAKACHUKWU T IDEOZU
+  'http://www.nass.gov.ng/mp/profile/826', # Hon. D Goodhead Boma
+  'http://www.nass.gov.ng/mp/profile/831', # Hon. Jerome Eke
+  'http://www.nass.gov.ng/mp/profile/836', # Hon. AWAJI-INOMBEK DAGOMIE ABIANTE
+  'http://www.nass.gov.ng/mp/profile/839', # Hon. KENNETH ANAYO CHIKERE
+  'http://www.nass.gov.ng/mp/profile/876', # Sen. GEORGE THOMPSON SEKIBO
+  'http://www.nass.gov.ng/mp/profile/884', # Hon. GOGO BRIGHT TAMUNO
+  'http://www.nass.gov.ng/mp/profile/891', # Sen. Ahmed .Salau Ogembe
+  'http://www.nass.gov.ng/mp/profile/896', # Hon. BROWN RANDOLPH IWO ONYERE
+  'http://www.nass.gov.ng/mp/profile/937', # Sen. OLAKA JOHNSON NWOGU
+  'http://www.nass.gov.ng/mp/profile/952', # Hon. Ugwuegede Ikechukwu
 ]
 
 def scrape(h)
